8ce52a1affbdd760f2e86cd9433452b1616a4018
Konstantin Suvorov <47966800+k-a-suvorov@users.noreply.github.com>
2020-02-16 19:41:30 +0300

    Add files via upload
    This program generate simple math examples and contains four math operators  +, -, *, /

---
+++ inputdef.py
@@ -0,0 +1,5 @@
+z1 = ""
+def inputResult (z1):
+	z1 = int(input(f'Please input your answer, if type \"stop\" - exit: '))
+	return z1
+			
---
+++ mathdefs.py
@@ -0,0 +1,12 @@
+z = 0
+
+if (z % 2 == 0):
+	print('The result is even! of the example.')
+else:
+	print('The result is odd of the example. ')	
+
+if (z > 0):
+	print('There is a positive result of the example.')
+elif (z < 0):
+	print('There is a negative result of the example.')	
+			
