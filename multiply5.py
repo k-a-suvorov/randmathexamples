8d9e4f2e046b6c81d9081db523d6b4241283050c
Konstantin Suvorov <47966800+k-a-suvorov@users.noreply.github.com>
2020-02-25 17:20:22 +0300

    Update multiply5.py

--- multiply5.py
+++ multiply5.py
@@ -1,44 +1,61 @@
-try:
-	import random
+#Конструкция для обработки всяких ошибок]
 
+try:
+	import random # библиотека случвйных значений
+
+	#Ввод значений
 	x = int(input('x = '))
 	y = int(input('y = '))
+	
+	#Проверка условий ввода	
+	if ((x > y) or (x == y)):
+		print('Не верное введены значения')
+	else:	
+		#Генерация случайного числа
+		a = random.randint(x,y)
 
-	a = random.randint(x,y)
-
+	#Функция вывода 0
 	def zeroPrint():
 		global a
 		z = 0
-		print(f'Zero {a}', z)
+		print(F'Zero {z} ', end='')
 	
-	if ((x > y) or (x == y)):
-		print('Не верное введены значения')
-	elif (y > x):
-			
+	
+	if (y > x):
+		
+		#вывод кратного числа 		
 		if (a % 5 == 0):
 			print('!!!')
 		elif (a % 5 != 0):
+			#Если не кратно - 0
 			print (a)
 			zeroPrint()
-
+	
 	print(a)
+	
+	#Вывод всех чисел кратных 5
+	for i in range(x, y, 1):
+		if (i % 5 == 0):
+			print(" ", i, end="")
+		elif (i % 5 != 0):
+			zeroPrint()	
 
-except Exception:
+#Если y равен 0
+except Exception: 
 	if (y == 0):
 		print(f'Это что ещё такое? {y} не должен быть 0')
 	else:
 		zeroPrint()
-except ZeroDivisionError:
-    print('On zero share cannot be!')		
+# Обработка ошибок вводимых значений		
 except ValueError:
-    print("You have some mistake of userinput Value!")
+    print("Некоректны вводимые значения")
+#Обработка типов переменных
 except TypeError:
-   print("You have some mistake of type Value!")
+   print("Некоректный тип данных")
+#Обработка системных ошибок
 except SystemError:
-	print("This is system mistake! Please don't panic! Relax!" )
-except ArithmeticError:
-	print("Arithmetic operations was broken!")
+	print("Обычная системная ошибка" )
+# Обработка ошибок загрузки модулей
 except ImportError:
-	print("Some modules not loaded, please check your source code!")
-except IOError:
-	print('An error IO file!')		
+	print("Нужные модули не загружены")
+
