8854ea2791e412d2e9314ac141aab15d02a88c07
Konstantin Suvorov <47966800+k-a-suvorov@users.noreply.github.com>
2020-02-25 17:02:41 +0300

    Update multiply5.py

--- multiply5.py
+++ multiply5.py
@@ -1,23 +1,44 @@
-import random # библиотека генерации случайного числа
-	#Ввод двуч чисел
 try:
+	import random
 
 	x = int(input('x = '))
-	y = int(input('x = '))
+	y = int(input('y = '))
 
 	a = random.randint(x,y)
-	z = 0
 
+	def zeroPrint():
+		global a
+		z = 0
+		print(f'Zero {a}', z)
+	
 	if ((x > y) or (x == y)):
 		print('Не верное введены значения')
-
+	elif (y > x):
 			
-	if (a % 5 == 0):
-		print('!!!')
-	else:
-		print(z)	
+		if (a % 5 == 0):
+			print('!!!')
+		elif (a % 5 != 0):
+			print (a)
+			zeroPrint()
 
 	print(a)
 
 except Exception:
-	print(f'Это что ещё такое? {y} не должен быть 0')
+	if (y == 0):
+		print(f'Это что ещё такое? {y} не должен быть 0')
+	else:
+		zeroPrint()
+except ZeroDivisionError:
+    print('On zero share cannot be!')		
+except ValueError:
+    print("You have some mistake of userinput Value!")
+except TypeError:
+   print("You have some mistake of type Value!")
+except SystemError:
+	print("This is system mistake! Please don't panic! Relax!" )
+except ArithmeticError:
+	print("Arithmetic operations was broken!")
+except ImportError:
+	print("Some modules not loaded, please check your source code!")
+except IOError:
+	print('An error IO file!')		
